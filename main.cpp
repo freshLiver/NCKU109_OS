9bb686b526238f2b2ea4cdf35f9803eed8e81732
freshLiver <freshliver.cys@gmail.com>
2020-10-02 23:22:17 +0800

    count program exec time

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "SortLargeFile.h"
 #include <cstdio>
+#include <ctime>
 
 int main ( int argc, char const *argv[] ) {
-    SortLargeFile test( argv[1], argv[2] );
+
+    time_t start = time ( NULL );
+    SortLargeFile test ( argv[1], argv[2] );
+    time_t end = time ( NULL );
+
+    printf ( "Total Cost : %ld\n", end - start );
     return 0;
 }
