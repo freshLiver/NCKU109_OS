3d2e4278ff5b6c688fd3712a7fd7ee98291ed446
freshLiver <freshliver.cys@gmail.com>
2020-10-09 19:23:18 +0800

    ReadCSV and TIME COST macro

--- HW2_CSV2JSON/main.cpp
+++ HW2_CSV2JSON/main.cpp
@@ -1,31 +1,67 @@
 #include "CSV2JSON.h"
+#include <ctime>
 #include <fstream>
 
-using std::ifstream;
-
+/**
+ * Convert char[] to int
+ * */
 static int CStr2Int32 ( const char *str ) {
     int result;
-
-
 
     return result;
 }
 
-int main ( int argc, char const *argv[] ) {
-    // handle args and open file
-    int threads = ( argv[2] == NULL ) ? 1 : CStr2Int32 ( argv[2] );
-    ifstream input ( argv[1], std::ios::in );
 
-    // get every line
-    int count = 0;        // TODO: MAX LINES ?
-    for ( string tmp; std::getline ( input, tmp ); ++count )
-        CSV2JSON::datas.push ( pair< int, string > ( count, tmp ) );
+/**
+ * read csv file and store to queue< pair<int,string> >
+ * */
+static int ReadCSV ( queue< pair< int, string > > &container, const char *target ) {
+    // use std::getline
+    std::ifstream fin ( target, std::ios::in );
+
+    int count = 0;
+    for ( string tmp; std::getline ( fin, tmp ); ++count )
+        container.push ( pair< int, string > ( count, tmp ) );
+
+    // close file
+    fin.close ( );
+
+    // result total lines
+    return count;
+}
+
+
+/**
+ * write every Value in Values[] into JSON file
+ * */
+static void WriteJSON ( pValues results ) {}
+
+int main ( int argc, char const *argv[] ) {
+    
+    // count from program start
+    time_t start;
+
+    // default 1 thread
+    int threads = ( argv[2] == NULL ) ? 1 : CStr2Int32 ( argv[2] );
+
+    // print infos
+    DEBUG ( "Input File : %s", argv[1] );
+    DEBUG ( " USE %d Threads ", threads );
+    DEBUG ( "Output File : %s", argv[3] );
+
+
+    // read file and store to CSV2JSON::datas
+    RESET_TIME ( );
+    int count = ReadCSV ( CSV2JSON::datas, argv[1] );
+    COST_TIME ( "Read CSV File" );
 
     // build class
     CSV2JSON cvs2json ( count, threads );
 
-    // 
+    // write every Value in CSV2JSON::result into JSON file
+    RESET_TIME ( );
+    WriteJSON ( CSV2JSON::results );
+    COST_TIME ( "Write JSON File" );
 
-    
     return 0;
 }
