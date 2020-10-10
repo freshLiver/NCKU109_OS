735a12fb4306ea7967efef8de646734484a6f672
freshLiver <freshliver.cys@gmail.com>
2020-10-10 17:32:12 +0800

    mv read csv to class ctor

--- HW2_CSV2JSON/main.cpp
+++ HW2_CSV2JSON/main.cpp
@@ -2,66 +2,54 @@
 #include <ctime>
 #include <fstream>
 
-/**
- * Convert char[] to int
- * */
-static int CStr2Int32 ( const char *str ) {
-    int result;
+#define OUTPUT_PATH "./output"
 
-    return result;
+typedef unsigned int uint;
+
+/**
+ * convert char * string into uint32
+ * */
+static uint c_str2int ( const char *c_str ) {
+    // if null
+    if ( c_str == NULL )
+        return 1;
+
+    uint value = 0;
+    // convert to int
+    for ( int i = 0; c_str[i] != '\0'; ++i ) {
+        value *= 10;
+        value += ( c_str[i] - '0' );
+    }
+
+    return ( value == 0 ) ? 1 : value;
 }
 
 
 /**
- * read csv file and store to queue< pair<int,string> >
+ * Main Function
  * */
-static int ReadCSV ( queue< pair< int, string > > &container, const char *target ) {
-    // use std::getline
-    std::ifstream fin ( target, std::ios::in );
-
-    int count = 0;
-    for ( string tmp; std::getline ( fin, tmp ); ++count )
-        container.push ( pair< int, string > ( count, tmp ) );
-
-    // close file
-    fin.close ( );
-
-    // result total lines
-    return count;
-}
-
-
-/**
- * write every Value in Values[] into JSON file
- * */
-static void WriteJSON ( pValues results ) {}
-
 int main ( int argc, char const *argv[] ) {
-    
-    // count from program start
-    time_t start;
+    // time counting
+    time_t start = time ( NULL );
 
-    // default 1 thread
-    int threads = ( argv[2] == NULL ) ? 1 : CStr2Int32 ( argv[2] );
+    /** handle args
+     *  @ argv[1] : input file path
+     *  @ argv[2] : number of threads
+     * */
 
-    // print infos
-    DEBUG ( "Input File : %s", argv[1] );
-    DEBUG ( " USE %d Threads ", threads );
-    DEBUG ( "Output File : %s", argv[3] );
+    string input ( argv[1] );
+    string output ( OUTPUT_PATH );
+
+    uint threads = c_str2int ( argv[2] );
 
 
-    // read file and store to CSV2JSON::datas
-    RESET_TIME ( );
-    int count = ReadCSV ( CSV2JSON::datas, argv[1] );
-    COST_TIME ( "Read CSV File" );
+    DEBUG ( "input file : %s", input.c_str ( ) );
+    DEBUG ( "threads    : %d", threads );
+    DEBUG ( "input file : %s", output.c_str ( ) );
 
-    // build class
-    CSV2JSON cvs2json ( count, threads );
+    // read and initialize class with datas and threads
+    CSV2JSON cvs2json ( input, output, (threads == 1) ? 1 : ( threads - 1 ) );
 
-    // write every Value in CSV2JSON::result into JSON file
-    RESET_TIME ( );
-    WriteJSON ( CSV2JSON::results );
-    COST_TIME ( "Write JSON File" );
 
     return 0;
 }
