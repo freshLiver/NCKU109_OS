b1ef2ecfc7e9d2073579c79c8829cbe8e846d1cd
freshLiver <freshliver.cys@gmail.com>
2020-11-25 01:00:23 +0800

    split ParseCmd Task, TODOs: read n lines, parse key index

--- HW3_Key_Value_Stroages/KeyValueStorage.h
+++ HW3_Key_Value_Stroages/KeyValueStorage.h
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <exception>
 #include <fstream>
 #include <queue>
 #include <string>
@@ -15,67 +16,94 @@ using std::queue;
 using std::string;
 using std::thread;
 using std::tuple;
+using std::get;
 
 class KeyValueStorage {
 private:
-    /* data */
+    static int GetKeyFromCmd ( string &cmd, string &key, int start, char delim ) {
+        for ( int iCmd = start, iKey = 0;; ++iCmd, ++iKey ) {
+            if ( cmd[iCmd] == delim )
+                return ++iCmd;
+            else
+                key[iKey] = cmd[iCmd];
+        }
+    }
+    static void GetValueFromCmd ( string &cmd, string &value, int start ) {
+        for ( int iCmd = start, iValue = 0; iValue < 128; ++iCmd, ++iValue )
+            value[iValue] = cmd[iCmd];
+    }
+    //
+    // Parse This Command As Given CmdType
+    //
+    static tuple<string, string> ParseCommandAs ( CmdType type, string cmd ) {
+        string key ( 20, '\0' ), value ( 129, '\0' );
+        int end;
+        switch ( type ) {
+            // * GET  : GET key[<20]
+            case PUT:
+                end = GetKeyFromCmd ( cmd, key, 4, ' ' );
+                GetValueFromCmd ( cmd, value, end );
+                break;
+
+            // * PUT  : PUT key[<20] value[128]
+            case GET:
+                GetKeyFromCmd ( cmd, key, 4, '\0' );
+                break;
+
+            // * SCAN : SCAN key1[<20] key2[<20]
+            case SCAN:
+                end = GetKeyFromCmd ( cmd, key, 5, ' ' );
+                GetKeyFromCmd ( cmd, value, end, '\0' );
+                break;
+        }
+        return std::make_tuple ( key, value );
+    }
+
 public:
     //
     // thread putThreads[10];
     // queue< tuple< LL, char[128] > > putCommands[10];
     KeyValueStorage ( string input );
 
+
+    //
+    // Read Input File
+    // TODO: Read N lines once, until EOF
+    //
     static void ReadInputFile ( string input ) {
         std::ifstream fin ( input.c_str ( ), std::ios::in );
         int count = 1;
         for ( string tmp; std::getline ( fin, tmp ); ++count ) {
             int type, index;
             string key, value;
-            auto result = ParseCommand ( tmp );
-            DEBUG ( "line[%3d] : type: %d, index: %d\nkey: %s\nvalue: %s\n",
+            auto res = ParseCommand ( tmp );
+            DEBUG ( "line[%3d](%d) :\n\tkey: %s\n\tvalue: %s\n",
                     count,
-                    std::get< 0 > ( result ),
-                    std::get< 1 > ( result ),
-                    std::get< 2 > ( result ).c_str ( ),
-                    std::get< 3 > ( result ).c_str ( ) );
+                    get<0> ( res ),
+                    get<1> ( res ).c_str ( ),
+                    get<2> ( res ).c_str ( ) );
         }
     }
-
-    static tuple< int, int, string, string > ParseCommand ( string cmd ) {
-        int type, index;
-        char key[20], value[129] = "\0";
-
+    //
+    // Parse This Command, and return its informations
+    //
+    static tuple<CmdType, string, string> ParseCommand ( string cmd ) {
+        CmdType type;
         // parse command type
-        int iCmd = 4;
-        if ( cmd[0] == 'P' )
-            type = PUT;
-        else if ( cmd[0] == 'G' )
-            type = GET;
-        else {
-            type = SCAN;
-            iCmd = 5;
-        }
-
-        // read key and key index
-        for ( int iKey = 0;; ++iKey, ++iCmd ) {
-            // command format : "TYPE KEY VALUE"
-            if ( cmd[iCmd] == ' ' || cmd[iCmd] == '\0' ) {
-                // last char is index
-                index = cmd[iCmd - 1] - '0';
-                // make string end
-                key[iKey] = '\0';
-                ++iCmd;
+        switch ( cmd[0] ) {
+            case 'P':
+                type = PUT;
                 break;
-            }
-            key[iKey] = cmd[iCmd];
+            case 'G':
+                type = GET;
+                break;
+            case 'S':
+                type = SCAN;
+                break;
+            default:
+                throw new std::runtime_error ( "Invalid Command, not PUT, GET or SCAN.\n" );
         }
-
-        if ( type == PUT ) {
-            // read value
-            for ( int iValue = 0; iValue < 128; ++iValue, ++iCmd )
-                value[iValue] = cmd[iCmd];
-            value[128] = '\0';
-        }
-        return tuple< int, int, string, string > ( type, index, string ( key ), string ( value ) );
+        auto info = ParseCommandAs ( type, cmd );
+        return std::make_tuple ( type, get<0> ( info ), get<1> ( info ) );
     }
 };
--- HW3_Key_Value_Stroages/macros.h
+++ HW3_Key_Value_Stroages/macros.h
@@ -1,6 +1,8 @@
-#define DEBUG( format, ... ) printf ( "DEBUG >> " format "\n", __VA_ARGS__ )
-typedef long long LL;
+#define DEBUG( format, ... ) printf ( "DEBUG >>> " format "------- DEBUG END\n\n", __VA_ARGS__ )
 
 #define PUT 1
 #define GET 2
 #define SCAN 3
+
+typedef int CmdType;
+typedef long long LL;
