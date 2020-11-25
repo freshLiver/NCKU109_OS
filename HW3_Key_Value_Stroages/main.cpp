b79a1343341d21343881ad629caa89b5c37ba2b6
freshLiver <freshliver.cys@gmail.com>
2020-11-25 12:52:58 +0800

    count runtime, get output file name

--- HW3_Key_Value_Stroages/main.cpp
+++ HW3_Key_Value_Stroages/main.cpp
@@ -1,11 +1,47 @@
 #include "KeyValueStorage.h"
+#include <ctime>
 
+//
+// Convert input file name to output file name by change .input -> .output
+//
+string GetOutputName ( string inputName ) {
+    string res ( inputName.size ( ) + 1, '\0' );
+    int dotEnd = inputName.size ( ) - 5;
 
+    // copy string before "input"
+    for ( int i = 0; i < dotEnd; ++i )
+        res[i] = inputName[i];
+
+    // copy ".input" and right shift 1 char
+    for ( int i = dotEnd; i < res.size ( ); ++i )
+        res[i] = inputName[i - 1];
+
+    // .in -> out
+    res[dotEnd++] = 'o';
+    res[dotEnd++] = 'u';
+    res[dotEnd++] = 't';
+
+    return res;
+}
+//
+// MAIN, input format is : ./executable ./inputfile.input
+//
 int main ( int argc, char const *argv[] ) {
-    /* input format : ./exec ./file.input */
+    time_t start, finish;
 
+    // start counting time
+    start = time ( NULL );
 
-    KeyValueStorage ( string ( argv[1] ) );
+    // get input and output file name with ext as std::string
+    string inputFilename ( argv[1] );
+    string outputFileName ( GetOutputName ( inputFilename ) );
+
+    // pass input and output to KVS instance
+    KeyValueStorage ( inputFilename, outputFileName );
+
+    // get finish time, and calc time cost
+    finish = time ( NULL );
+    printf ( "Total Time Cost : %ld secs.\n", finish - start );
 
     return 0;
 }
