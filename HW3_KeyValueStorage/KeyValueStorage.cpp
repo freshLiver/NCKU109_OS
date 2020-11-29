d183372ba86acece394f90257957acc4d5b969e2
freshLiver <freshliver.cys@gmail.com>
2020-11-29 16:10:23 +0800

    implement static function in .cpp, touch file to create db

--- HW3_KeyValueStorage/KeyValueStorage.cpp
+++ HW3_KeyValueStorage/KeyValueStorage.cpp
@@ -1,69 +1,98 @@
 #include "KeyValueStorage.h"
-#include "DBIO.h"
-#include <unistd.h>
+
+using std::get;
+using std::make_tuple;
+
+/***********************************************
+**************** public methods ****************
+***********************************************/
+
 
 KeyValueStorage::KeyValueStorage( string input, string output ) {
+
     // get input and output file name
     KeyValueStorage::inputFile = input;
     KeyValueStorage::outputFile = output;
 
     // 檢查是否所有 db 檔案都存在，若不存在就需要建立檔案
-    // ! 必須在 db Worker 以前進行，以免重複開啟檔案
-    DBIO::CheckAllDB();
+    system( "mkdir ./db/" );
+    for ( int dbID = 0; dbID < 10; ++dbID ) {
+        string touchCmd = "touch ./db/db0";
+        touchCmd[touchCmd.length() - 1] = '0' + dbID;
+        system( touchCmd.c_str() );
+    }
 
-    // 初始化所有
-    
-    // 開始讀取檔案
+    // 初始化所有 worker 和 thread
+    for ( int id = 0; id < 10; ++id ) {
 
-}
-
-void KeyValueStorage::ReadCommands() {
-    // open input file
-    std::ifstream input( inputFile.c_str(), std::ios::in );
-
-    // end parsing thread with 2 situations : EOF, Line MAX_READ
-    bool eofStop = false, maxReadPause = false;
-
-    // ! one thread push to queue, the other thread push to worker queue until GET
-    queue<string> cmdQueue;
-    thread thParse( ParseOnReading, &cmdQueue, &eofStop, &maxReadPause );
-
-
-    while ( eofStop ) {
-        // read N commands from input file at once
-        int count = 0;
-        queue<string> cmdQueue;
-        for ( string tmp; count < MAX_READ && eofStop; ++count ) {
-            // read a line and push to cmdQueue
-            std::getline( input, tmp );
-            // push to command vector
-            cmdQueue.push( tmp );
-        }
     }
 }
-//
-//
-//
-void KeyValueStorage::ParseOnReading( queue<string> &cmdQueue, bool &stop, bool &pause ) {
-    while ( stop || pause ) {
-        while ( cmdQueue.size() < 0 ) {
-            // pop first element from queue
-            string cmd = cmdQueue.front();
-            cmdQueue.pop();
 
-            // parse this cmd and get cmd info
-            auto res = KeyValueStorage::ParseCommand( cmd );
-            CmdType type = get<CType>( res );
-            string key = get<1>( res ), value_key2 = get<2>( res );
 
-            // if PUT, just push to worker queue
-            if ( type == PUT ) {
-                int worker = key.back() - '0';
-                KeyValueStorage::workerCmds[worker].push( make_tuple( key, value_key2 ) );
-            }
-            // if SCAN or GET,
-            else {
-            }
-        }
+/************************************************
+**************** private methods ****************
+************************************************/
+
+
+int KeyValueStorage::GetKeyFromCmd( string &cmd, string &key, int start, char delim ) {
+    for ( int iCmd = start, iKey = 0;; ++iCmd, ++iKey ) {
+        if ( cmd[iCmd] == delim )
+            return ++iCmd;
+        else
+            key[iKey] = cmd[iCmd];
     }
+}
+
+
+void KeyValueStorage::GetValueFromCmd( string &cmd, string &value, int start ) {
+    for ( int iCmd = start, iValue = 0; iValue < 128; ++iCmd, ++iValue )
+        value[iValue] = cmd[iCmd];
+}
+
+
+tuple<string, string> KeyValueStorage::ParseCommandAs( CmdType type, string cmd ) {
+
+    string key( 20, '\0' ), value( 129, '\0' );
+    int end;
+    switch ( type ) {
+        // * GET  : GET key[<20]
+        case PUT:
+            end = GetKeyFromCmd( cmd, key, 4, ' ' );
+            GetValueFromCmd( cmd, value, end );
+            break;
+
+        // * PUT  : PUT key[<20] value[128]
+        case GET:
+            GetKeyFromCmd( cmd, key, 4, '\0' );
+            break;
+
+        // * SCAN : SCAN key1[<20] key2[<20]
+        case SCAN:
+            end = GetKeyFromCmd( cmd, key, 5, ' ' );
+            GetKeyFromCmd( cmd, value, end, '\0' );
+            break;
+    }
+    return make_tuple( key, value );
+}
+
+
+tuple<CmdType, string, string> KeyValueStorage::ParseCommand( string cmd ) {
+
+    CmdType type;
+    // parse command type
+    switch ( cmd[0] ) {
+        case 'P':
+            type = PUT;
+            break;
+        case 'G':
+            type = GET;
+            break;
+        case 'S':
+            type = SCAN;
+            break;
+        default:
+            throw new std::runtime_error( "Invalid Command, not PUT, GET or SCAN.\n" );
+    }
+    auto info = ParseCommandAs( type, cmd );
+    return make_tuple( type, get<0>( info ), get<1>( info ) );
 }
--- HW3_KeyValueStorage/KeyValueStorage.h
+++ HW3_KeyValueStorage/KeyValueStorage.h
@@ -1,8 +1,8 @@
+#if !defined( KEYVALUESTORAGE_H )
+#define KEYVALUESTORAGE_H
+
 #include "macros.h"
-#include <algorithm>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include <exception>
 #include <fstream>
 #include <queue>
@@ -10,103 +10,43 @@
 #include <thread>
 #include <tuple>
 
-using std::get;
-using std::make_tuple;
 using std::queue;
 using std::string;
 using std::thread;
 using std::tuple;
+using std::fstream;
 
 class KeyValueStorage {
 
-  public:
-    //
-    // ctor :
-    // * 1. get input and output file name
-    // * 2. check if db files exist and init dbFiles[]
-    // * 3.
-    //
-    KeyValueStorage( string input, string output );
-    //
-    // Read N Lines At Once From Input File, Until EOF
-    //
-    static void ReadCommands();
-    //
-    // should parse command in cmdQueue and push to resp until GET or SCAN while Reading
-    //
-    static void ParseOnReading();
-
-  private:
-    //
+public:
+private:
     static string inputFile, outputFile;
     static thread dbWorkers[10];
-    static std::ofstream dbFiles[10];
+    static fstream dbFiles[10];
     static queue<tuple<string, string>> workerCmds[10];
 
-    //
-    //
-    //
-    static int GetKeyFromCmd( string &cmd, string &key, int start, char delim ) {
-        for ( int iCmd = start, iKey = 0;; ++iCmd, ++iKey ) {
-            if ( cmd[iCmd] == delim )
-                return ++iCmd;
-            else
-                key[iKey] = cmd[iCmd];
-        }
-    }
-    //
-    //
-    //
-    static void GetValueFromCmd( string &cmd, string &value, int start ) {
-        for ( int iCmd = start, iValue = 0; iValue < 128; ++iCmd, ++iValue )
-            value[iValue] = cmd[iCmd];
-    }
-    //
-    // Parse This Command As Given CmdType
-    //
-    static tuple<string, string> ParseCommandAs( CmdType type, string cmd ) {
-        string key( 20, '\0' ), value( 129, '\0' );
-        int end;
-        switch ( type ) {
-            // * GET  : GET key[<20]
-            case PUT:
-                end = GetKeyFromCmd( cmd, key, 4, ' ' );
-                GetValueFromCmd( cmd, value, end );
-                break;
 
-            // * PUT  : PUT key[<20] value[128]
-            case GET:
-                GetKeyFromCmd( cmd, key, 4, '\0' );
-                break;
+public:
+    KeyValueStorage( string input, string output );
 
-            // * SCAN : SCAN key1[<20] key2[<20]
-            case SCAN:
-                end = GetKeyFromCmd( cmd, key, 5, ' ' );
-                GetKeyFromCmd( cmd, value, end, '\0' );
-                break;
-        }
-        return make_tuple( key, value );
-    }
-    //
-    // Parse This Command, and return its informations
-    //
-    static tuple<CmdType, string, string> ParseCommand( string cmd ) {
-        CmdType type;
-        // parse command type
-        switch ( cmd[0] ) {
-            case 'P':
-                type = PUT;
-                break;
-            case 'G':
-                type = GET;
-                break;
-            case 'S':
-                type = SCAN;
-                break;
-            default:
-                throw new std::runtime_error( "Invalid Command, not PUT, GET or SCAN.\n" );
-        }
-        auto info = ParseCommandAs( type, cmd );
-        return make_tuple( type, get<0>( info ), get<1>( info ) );
-    }
+
+    static void ReadNCommands( fstream &input, int N, string cmdBuffer[] );
+
+
+    static tuple<CmdType, int> QuickParseCmd( string &cmd ) {}
+
+
+private:
+    static int GetKeyFromCmd( string &cmd, string &key, int start, char delim );
+
+
+    static void GetValueFromCmd( string &cmd, string &value, int start );
+
+
+    static tuple<string, string> ParseCommandAs( CmdType type, string cmd );
+
+
+    static tuple<CmdType, string, string> ParseCommand( string cmd );
 };
+
+#endif        // KEYVALUESTORAGE_H
